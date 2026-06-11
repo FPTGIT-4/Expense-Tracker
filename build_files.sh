1e4b7bbf4eeed0ca2c8146918cb13ae49b785629
FAYIZ <fayizptofficial@gmail.com>
2026-06-11 16:14:37 +0530

    Remove migrate from Vercel build

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "Building the project..."
-python3 -m pip install -r requirements.txt
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
-
-echo "Running database migrations..."
-python3 manage.py migrate --noinput
+python manage.py collectstatic --noinput
